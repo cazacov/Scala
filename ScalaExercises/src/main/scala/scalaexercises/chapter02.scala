eb13a3a57148adea3dbac0cc3c46b4ce799587bb
Victor Cazacov <cazacov.git@gmail.com>
2014-12-21 00:15:23 +0100

    Solved exercises 2.2

--- ScalaExercises/src/main/scala/scalaexercises/chapter02.scala
+++ ScalaExercises/src/main/scala/scalaexercises/chapter02.scala
@@ -1,12 +1,29 @@
 package scalaexercises
 
 class chapter02 {
-  // Exercise 2.1
 
+  // Exercise 2.1
   def fib(n: Int): Int =
     if (n == 0)
       0
     else if (n == 1)
       1
-    else fib(n-2) + fib(n-1)
+    else fib(n - 2) + fib(n - 1)
+
+  // Exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def check(prevElem: A, i: Int): Boolean = {
+      if (i >= as.length)
+        true
+      else if (!ordered(prevElem, as(i)))
+        false
+      else
+        check(as(i), i+1)
+    }
+
+    if (as.length == 0)
+      true
+    else
+      check(as(0), 1)
+  }
 }
--- ScalaExercises/src/test/scala/scalaexercises/tests/chapter02Test.scala
+++ ScalaExercises/src/test/scala/scalaexercises/tests/chapter02Test.scala
@@ -3,10 +3,7 @@ package scalaexercises.tests
 import scalaexercises.chapter02
 import org.scalatest.FunSuite
 
-/**
- * Created by Papa on 08.12.2014.
- */
-class chapter02Test extends FunSuite {
+class chapter02Exercise21 extends FunSuite {
   test("Fib(0) returns 0") {
     val ch = new chapter02
     val actual = ch.fib(0)
@@ -27,4 +24,45 @@ class chapter02Test extends FunSuite {
     val expected = 13
     assert(expected == actual)
   }
+}
+
+class chapter02Exercise22 extends FunSuite {
+  val ch = new chapter02
+
+  test("isSorted on empty array returns true") {
+    var array = Array[Int]()
+    var actual = ch.isSorted(array, (x:Int, y:Int) => x < y)
+    assert(true == actual)
+  }
+
+  test("isSorted on one-element array returns true") {
+    var array = Array(1)
+    var actual = ch.isSorted(array, (x:Int, y:Int) => x < y)
+    assert(true == actual)
+  }
+
+  test("isSorted on sorted array returns true") {
+    var array = Array(1, 2, 3)
+    var actual = ch.isSorted(array, (x:Int, y:Int) => x < y)
+    assert(true == actual)
+  }
+
+  test("isSorted on unsorted array returns false") {
+    var array = Array(1, 3, 2)
+    var actual = ch.isSorted(array, (x:Int, y:Int) => x < y)
+    assert(false == actual)
+  }
+
+  test("isSorted on sorted char array returns true") {
+    var array = "abcdef".toCharArray()
+    var actual = ch.isSorted(array, (x:Char, y:Char) => x < y)
+    assert(false == actual)
+  }
+
+  test("isSorted on unsorted char array returns false") {
+    var array = "scala".toCharArray()
+    var actual = ch.isSorted(array, (x:Char, y:Char) => x < y)
+    assert(false == actual)
+  }
+
 }
