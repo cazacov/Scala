d88620690e4dcfb157ac634e41f54945bfeab068
Victor Cazacov <cazacov.git@gmail.com>
2014-12-23 23:42:32 +0100

    Working on Chapter 3

--- ScalaExercises/src/main/scala/scalaexercises/chapter03.scala
+++ ScalaExercises/src/main/scala/scalaexercises/chapter03.scala
@@ -11,9 +11,37 @@ class chapter03 {
     case h :: t => t
   }
 
+  // Exercise 3.3
   def setHead[A](l: List[A], newHead: A): List[A] = l match {
     case Nil => throw new IllegalArgumentException("list cannot be empty")
     case h :: t => newHead :: t
+  }
+
+  // Exercise 3.4
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n == 0)
+      l
+    else
+      l match {
+        case Nil => throw new IllegalArgumentException("list cannot be empty")
+        case h :: t => drop(t, n-1)
+      }
+
+  // Exercise 3.5
+  def dropWhile[A](l: List[A])(predicate: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case h :: t =>
+      if (predicate(h))
+        dropWhile(t)(predicate)
+      else
+        l
+  }
+
+  // Exercise 3.6
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => throw new IllegalArgumentException("list cannot be empty")
+    case h :: Nil => Nil
+    case h :: t => h :: init(t)
   }
 
 }
--- ScalaExercises/src/test/scala/scalaexercises/tests/chapter03Test.scala
+++ ScalaExercises/src/test/scala/scalaexercises/tests/chapter03Test.scala
@@ -6,14 +6,72 @@ import scalaexercises.chapter03
 import org.scalatest.FunSuite
 
 // Exercise 3.2
-class chapter03Test extends FunSuite with ShouldMatchers{
+class chapter03Exercise32Test extends FunSuite with ShouldMatchers {
   val ch = new chapter03
 
-  test("tail function returns list tail")  {
-    ch.tail(List(1, 2, 3)) should equal (List(2, 3))
+  test("tail function returns list tail") {
+    ch.tail(List(1, 2, 3)) should equal(List(2, 3))
   }
 
-  test("setHead function replaces the first element") {
-    ch.setHead(List(1, 2, 3), 4) should equal (List(4, 2, 3))
+  test("tail function throws an execption if the list is empty") {
+    an[IllegalArgumentException] should be thrownBy ch.tail(List())
   }
 }
+
+// Exercise 3.3
+class chapter03Exercise33Test extends FunSuite with ShouldMatchers {
+  val ch = new chapter03
+
+  test("setHead function replaces the first element") {
+    ch.setHead(List(1, 2, 3), 4) should equal(List(4, 2, 3))
+  }
+
+  test("setHead throws an exception if the list is empty") {
+    an[IllegalArgumentException] should be thrownBy ch.setHead(List(), 42)
+  }
+}
+
+// Exercise 3.4
+class chapter03Exercise34Test extends FunSuite with ShouldMatchers {
+  val ch = new chapter03
+
+  test("drop skip first n elements") {
+    ch.drop(List(1, 2, 3, 4, 5, 6), 3) should equal (List(4, 5, 6))
+  }
+
+  test("drop throws an exception if list is shorter than n") {
+    an [IllegalArgumentException] should be thrownBy ch.drop(List(1, 2), 3)
+  }
+}
+
+// Exercise 3.5
+class chapter03Exercise35Test extends FunSuite with ShouldMatchers {
+  val ch = new chapter03
+
+  test("dropWhile skips elements while predicate is true") {
+    ch.dropWhile(List(1, 2, 3, 4, 5, 6))(x => x < 4) should equal (List(4, 5, 6))
+  }
+
+  test("dropWhile returns an empty list if the predicate is always true") {
+    ch.dropWhile(List(1, 2, 3, 4, 5, 6))(x => true) should equal (List())
+  }
+
+  test("dropWhile returns an empty list if the input list is empty") {
+    ch.dropWhile(List())(x => true) should equal (List())
+  }
+}
+
+
+// Exercise 3.6
+class chapter03Exercise36Test extends FunSuite with ShouldMatchers {
+  val ch = new chapter03
+
+  test("init returns all list elements but the last one") {
+    ch.init(List(1, 2, 3, 4)) should equal (List(1,2,3))
+  }
+
+  test("init throws an exception if the list is empty") {
+    an [IllegalArgumentException] should be thrownBy ch.init(List())
+  }
+}
+
